dbaba6996008c4ff1c24efdc722cf2ae456c2d84
prakash-er <prakash@pipecandy.com>
2022-01-31 21:40:07 +0530

    added post

--- Contorllers/crudController.js
+++ Contorllers/crudController.js
@@ -19,4 +19,53 @@ const createStudent = {
     }
 }
 
-module.exports = createStudent
+//read
+const readAllData = {
+    readData : async (req,res) => {
+        try {
+            const data = await CrudData.find({})
+            res.status(200).json(data)
+        } catch (error) {
+            res.status(500).json({msg: "Internal server error"})
+        }
+        
+    }
+}
+
+//Update data
+const updateData = {
+    newUpdatedData : async (req,res) => {
+        try {
+            const newStudentName = req.body.name
+            const id = req.body.id
+
+                await CrudData.findById(id, (error, newUpdatedStudentName) => {
+                newUpdatedStudentName.name = newStudentName
+                newUpdatedStudentName.save()
+                res.status(200).json({msg: "Data Updated "})
+            })
+        } catch (error) {
+            res.status(500).json({msg: "Internal server error"})
+            console.log(error);
+        }
+        
+    }
+}
+
+//delete data
+const deleteData = {
+    removeData : async (req,res) => {
+        try {
+            const id = req.params.id
+
+            await CrudData.findByIdAndRemove(id).exec()
+            res.status(200).json({msg:"Data Deleted"})
+
+        } catch (error) {
+            res.status(500).json({msg: "Internal server error"})
+        }
+        
+    }
+}
+
+module.exports = {createStudent,readAllData, updateData, deleteData}
--- Routes/crudRoute.js
+++ Routes/crudRoute.js
@@ -1,6 +1,13 @@
 const router = require('express').Router()
-const createStudent = require("../Contorllers/crudController")
+const {createStudent, readAllData, updateData, deleteData} = require("../Contorllers/crudController")
 
 router.post("/create" ,createStudent.name)
+
+router.get("/read" ,readAllData.readData)
+
+router.put("/update/:id" ,updateData.newUpdatedData)
+
+router.delete("/delete/:id" ,deleteData.removeData)
+
 
 module.exports = router
